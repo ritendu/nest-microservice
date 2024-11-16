cfca1ce7a74549b55ce2751382417aed0d1c8d4b
ritendu <ritendusen2@gmail.com>
2024-11-16 19:22:34 +0530

    changes authentication

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import * as Joi from 'joi';
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot({ isGlobal: true }),],
+  imports: [UsersModule,  ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      MONGO_URI: Joi.string().required(),
+    }),
+  }),
+  JwtModule.registerAsync({
+    inject: [ConfigService], // Specify ConfigService as a dependency
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: {
+        expiresIn: `${configService.get<string>('JWT_EXPIRATION')}s`,
+      },
+    }),
+  }),
+],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
-
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3001);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
